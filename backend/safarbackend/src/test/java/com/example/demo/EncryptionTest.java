9b0235162695c209da03150fe3e97f1474adccb1
TDD66 <tadiwanshe.dzvoti@student.manchester.ac.uk>
2024-05-03 14:42:20 +0100

    Ran test

--- backend/safarbackend/src/test/java/com/example/demo/EncryptionTest.java
+++ backend/safarbackend/src/test/java/com/example/demo/EncryptionTest.java
@@ -1,25 +1,23 @@
 package com.example.demo;
 
-
-import com.example.demo.encryptor.StringCryptoConverter;
 import com.example.demo.models.User;
 import com.example.demo.repositories.UserRepository;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.logging.LoggerFactory;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.logging.Logger;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class EncryptionTest {
 
     @Autowired
     UserRepository userRepository;
-
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(StringCryptoConverter.class);
 
     private static final String NAME = "John";
     private static final String EMAIL = "john@example.com";
@@ -38,6 +36,7 @@ public class EncryptionTest {
     @Test
     public void readDecrypted() {
         User user = userRepository.findById(id).get();
+        System.out.println(user.getName());
         assertThat(user.getName()).isEqualTo(NAME);
         assertThat(user.getEmail()).isEqualTo(EMAIL);
         assertThat(user.getPassword()).isEqualTo(PASSWORD);
