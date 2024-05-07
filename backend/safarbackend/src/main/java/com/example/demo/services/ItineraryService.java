99f3b8d23c9087d670c11e496adf217f92188ec7
TDD66 <tadiwanshe.dzvoti@student.manchester.ac.uk>
2024-05-07 15:21:04 +0100

    Made itinerary service a @Service

--- backend/safarbackend/src/main/java/com/example/demo/services/ItineraryService.java
+++ backend/safarbackend/src/main/java/com/example/demo/services/ItineraryService.java
@@ -4,10 +4,12 @@ import com.example.demo.models.Itinerary;
 import com.example.demo.models.UpdateItineraryDTO;
 import com.example.demo.repositories.ItineraryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ItineraryService {
 
     private final ItineraryRepository itineraryRepository;
