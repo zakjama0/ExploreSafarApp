c565f31eb443fb5ea55fc169fb3d843be80fb754
TDD66 <79865373+TDD66@users.noreply.github.com>
2024-05-16 17:47:37 +0100

    NOT WORKING

--- backend/safarbackend/src/main/java/com/example/demo/controllers/UserController.java
+++ backend/safarbackend/src/main/java/com/example/demo/controllers/UserController.java
@@ -21,13 +21,13 @@ public class UserController {
 
     @GetMapping
     @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
-    public ResponseEntity<List<User>> getAllCustomers(){
+    public ResponseEntity<List<User>> getUsers(){
         List<User> users = userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> getCustomerById(@PathVariable Long id){
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
         Optional<User> User = userService.getUserById(id);
         if(User.isPresent()){
             return new ResponseEntity<>(User.get(), HttpStatus.OK);
@@ -36,16 +36,16 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<User> addCustomer(@RequestBody NewUserDTO newUserDTO){
-        User newCustomer = userService.saveUser(newUserDTO);
-        return new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
+    public ResponseEntity<User> addUser(@RequestBody NewUserDTO newUserDTO){
+        User newUser = userService.saveUser(newUserDTO);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
     @PatchMapping(value = "/{id}")
-    public ResponseEntity<User> updateCustomer(@PathVariable Long id, @RequestBody UserDTO userDTO){
-        Optional<User> updatedCustomer = userService.updateUser(id, userDTO);
-        if (updatedCustomer.isPresent()){
-            return new ResponseEntity<>(updatedCustomer.get(), HttpStatus.OK);
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO){
+        Optional<User> updatedUser = userService.updateUser(id, userDTO);
+        if (updatedUser.isPresent()){
+            return new ResponseEntity<>(updatedUser.get(), HttpStatus.OK);
         }
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
