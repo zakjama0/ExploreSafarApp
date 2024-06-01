2be09ec2b521d93f629c921cf8da9fda8f488860
zakjama0 <zakjama0@gmail.com>
2024-06-01 03:03:15 +0100

    Fetched halal restuarants alhamdulilah

---
+++ frontend/safarapp/src/components/SafarMap/apiIndex.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import axios from 'axios'
+
+const URL = 'https://map-places.p.rapidapi.com/nearbysearch/json';
+
+
+const options = {
+    params: {
+      location: '51.52478613001649, -0.13755916092148784',
+      radius: '1500',
+      keyword: 'halal',
+      type: 'restaurant'
+    },
+    headers: {
+      'x-rapidapi-key': "312917582fmsh44ae633e94bb69ap18d3fajsn8617488ed8c7", // Ensure your key is set in the environment variables
+      'x-rapidapi-host': 'map-places.p.rapidapi.com'
+    }
+  };
+  
+ 
+  
+export const getPlacesData = async () => {
+      try {
+        const response = await axios.get(URL, options);
+        console.log('Full response:', response); // Log the full response to see its structure
+        const { data } = response;
+        console.log('Data:', data); // Log the data to see if it contains the expected properties
+        return data; // Adjust based on the actual structure of the response
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        return []; // Return an empty array or handle the error as needed
+      }
+    };
+
+
--- frontend/safarapp/src/containers/MapsContainer.js
+++ frontend/safarapp/src/containers/MapsContainer.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { CssBaseline, Grid } from '@mui/material'
 import Search from '../components/SafarMap/Header/Search'
 import List from '../components/SafarMap/List/List'
 import Map from '../components/SafarMap/Maps/Map'
-
+import { getPlacesData } from '../components/SafarMap/apiIndex'
 const MapsContainer = () => {
+    const [places, setPlaces] = useState([])
+
+
+    useEffect(() => {
+      getPlacesData().then((data) => {
+        console.log('Returned data:', data);
+        setPlaces(data);
+      });
+    }, []);
+
+
   return (
     <div className='h-screen dark:bg-slate-800 dark:text-white w-full'>
         <CssBaseline/>
