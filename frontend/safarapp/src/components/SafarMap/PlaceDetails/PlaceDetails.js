7386b4251385e24b95668ad741d2c6a92b46e47a
zakjama0 <zakjama0@gmail.com>
2024-06-02 13:53:40 +0100

    Photos working

--- frontend/safarapp/src/components/SafarMap/PlaceDetails/PlaceDetails.js
+++ frontend/safarapp/src/components/SafarMap/PlaceDetails/PlaceDetails.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import Logo from '../../../assests/logoname.png'
 
-
-const staticPhotoReference = "AUGGfZlXes2edPoMmAdcAv1w2L-gVLa_-fNIwhM-CqKF0y74i6CsKVxk4D8dMeT1X5aNpzUyQ_5EmBKv4DDKB5GQkB9A-B3NCDZRT9xQoYlpvt_4gizxBkdGPelgzm2n1O605D_TDv5W8c_CCCh0YKIQbQ7e9e2aC0FQFPu53UjHqKAw7jFB"
 
 const getPhotoUrl = (photoReference) => {
-    return `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=AIzaSyD5OAaC5LrjisDtQOlgnXAoaWvbvGguVIc'`;
+    return `https://maps.googleapis.com/maps/api/place/photo?maxwidth=3264&maxheight=1837&photoreference=${photoReference}&key=AIzaSyD5OAaC5LrjisDtQOlgnXAoaWvbvGguVIc`;
   };
 
   const PlaceDetails = ({ place }) => {
@@ -13,7 +12,7 @@ const getPhotoUrl = (photoReference) => {
       <div>
         <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 transition-transform transform hover:-translate-y-2">
       <img 
-        src={place.photos && place.photos.length > 0 ? getPhotoUrl(place.photos[0].photo_reference) : 'https://via.placeholder.com/400'} 
+        src={place.photos && place.photos.length > 0 ? getPhotoUrl(place.photos[0].photo_reference) : {Logo} } 
         alt={place.name} 
         className="w-full h-48 object-cover"
       />
