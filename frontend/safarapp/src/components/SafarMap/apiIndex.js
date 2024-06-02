68f99e3235240dff2c9ee61b72e50867f5707d28
zakjama0 <zakjama0@gmail.com>
2024-06-02 12:20:21 +0100

    Adding card details

--- frontend/safarapp/src/components/SafarMap/apiIndex.js
+++ frontend/safarapp/src/components/SafarMap/apiIndex.js
@@ -3,21 +3,6 @@ import axios from 'axios'
 
 const URL = 'https://map-places.p.rapidapi.com/nearbysearch/json';
 
-
-const options = {
-    params: {
-      location: '51.52478613001649, -0.13755916092148784',
-      radius: '1500',
-      keyword: 'halal',
-      type: 'restaurant'
-    },
-    headers: {
-      'x-rapidapi-key': "312917582fmsh44ae633e94bb69ap18d3fajsn8617488ed8c7", // Ensure your key is set in the environment variables
-      'x-rapidapi-host': 'map-places.p.rapidapi.com'
-    }
-  };
-  
- 
   
 export const getPlacesData = async ({coordinates}) => {
       try {
