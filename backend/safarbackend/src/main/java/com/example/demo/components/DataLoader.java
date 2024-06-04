7ba4f98d71406a2651c787a3fb3dd9c8ceefbac9
TDD66 <79865373+TDD66@users.noreply.github.com>
2024-06-04 14:14:52 +0100

    .

--- backend/safarbackend/src/main/java/com/example/demo/components/DataLoader.java
+++ backend/safarbackend/src/main/java/com/example/demo/components/DataLoader.java
@@ -48,7 +48,7 @@ public class DataLoader implements ApplicationRunner {
                 .build();
 
         userRepository.save(user);
-
+S
 //        Dua
         Dua startJourney = new Dua("Starting the journey","بِسْمِ اللَّهِ تَوَكَّلْتُ عَلَى اللَّهِ لاَ حَوْلَ وَلاَ قُوَّةَ إِلاَّ بِاللَّهِ","In the name of Allah, I trust in Allah; there is no might and no power but in Allah" ,"Bismillahi tawakkaltu alallahi la hawla wala quwwata illa billah", "For the following verse, Anas bin Malik (RA) narrates that Prophet Muhammad (PBUH) said that: “Whoever says upon leaving his house (recites the below-mentioned verses) it will be said to him: ‘You are guided, defended and protected.’ The devil will go far away from him.”" ,"Grade: Sahih (Al-Albani)\n" +
                 "Reference: Sunan Abi Dawud 5095\n" +
