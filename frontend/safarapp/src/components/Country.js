610321ee36a31449068d1ce2998000e9336e0053
TDD66 <79865373+TDD66@users.noreply.github.com>
2024-06-06 20:11:33 +0100

    Formatted

--- frontend/safarapp/src/components/Country.js
+++ frontend/safarapp/src/components/Country.js
@@ -25,22 +25,21 @@ const Country = ({ cities }) => {
 
         <div className="bg-[#d2dbd8] h-full pb-40 dark:bg-slate-800 dark:text-white w-full">
             <div
-      className="block w-full bg-white bg-cover p-20 shadow-lg dark:bg-neutral-700"
-      style={{
-        backgroundImage: `url(${country.image})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
-      <h1 className="font-bold italic text-6xl text-slate-200  text-center">{country.name}</h1>
-
+                className="block w-full bg-white bg-cover p-20 shadow-lg dark:bg-neutral-700"
+                style={{
+                    backgroundImage: `url(${country.image})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+            >
+                <h1 className="font-bold italic text-6xl text-slate-200  text-center">{country.name}</h1>
             </div>
             <Box>
                 <h5 class=" text-center my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white pl-10">Top Things To Do:</h5>
                 <FeaturedAttractionList attractions={attractions.slice(0, 3)} />
             </Box>
             <Box sx={{ p: 3 }}>
-                <AttractionList cities={filteredCities}/>
+                <AttractionList cities={filteredCities} />
             </Box>
         </div>
     );
