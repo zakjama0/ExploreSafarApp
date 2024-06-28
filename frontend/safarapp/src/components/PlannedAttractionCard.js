cff5283bc91c01364d098dd2cbaaa1bde7d20a26
TDD66 <79865373+TDD66@users.noreply.github.com>
2024-06-28 15:55:53 +0100

    Changed to Card and added grid container to planned attraction card

--- frontend/safarapp/src/components/PlannedAttractionCard.js
+++ frontend/safarapp/src/components/PlannedAttractionCard.js
@@ -1,26 +1,17 @@
 import { Grid } from "@mui/material";
+import Card from "./Card";
 
 const PlannedAttractionCard = ({ name, image, startDate }) => {
 
     return (
-        <>
+        <Grid columns={2} container>
             <Grid item>
                 <h1>{startDate}</h1>
             </Grid>
             <Grid item>
-                <div class="bg-[#d2dbd8] max-w-sm m-4 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg w-full h-60" src={image} alt="" />
-                    </a>
-                    <div class="p-5">
-                        <a href="#">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
-                        </a>
-
-                    </div>
-                </div>
+                <Card name={name} image={image} />
             </Grid>
-        </>
+        </Grid>
     );
 
 }
