6cd3b2c2d727f2f6a8c34360545b3b8c99486816
TDD66 <79865373+TDD66@users.noreply.github.com>
2024-06-28 15:39:58 +0100

    Optimised router

--- frontend/safarapp/src/containers/MainContainer.js
+++ frontend/safarapp/src/containers/MainContainer.js
@@ -267,13 +267,11 @@ const MainContainer = () => {
                 {
                     path: "/countries/:countryId",
                     loader: countryLoader,
-                    errorElement: <h1>NOT FOUND</h1>,
                     element: <Country cities={cities} postPlannedAttraction={postPlannedAttraction} />
                 },
                 {
                     path: "/attractions/:attractionId",
                     loader: attractionLoader,
-                    errorElement: <h1>NOT FOUND</h1>,
                     element: <Attraction
                         cities={cities}
                         postPlannedAttraction={postPlannedAttraction}
@@ -289,7 +287,6 @@ const MainContainer = () => {
                 {
                     path: "my-itineraries/:itineraryId",
                     loader: myItineraryLoader,
-                    errorElement: <h1>NOT FOUND</h1>,
                     element: <MyItineraryContainer />
                 },
                 {
